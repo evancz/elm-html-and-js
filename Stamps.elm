b1f4f2d9b51d8d2bbbf9ec97bd9b361bdf878aed
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2014-12-22 05:42:20 +0000

    Update to Elm 0.14

--- Stamps.elm
+++ Stamps.elm
@@ -2,6 +2,13 @@ module Stamps where
 
 import Mouse
 import Window
+import Signal (..)
+import List ((::), length)
+import List
+import Graphics.Collage (..)
+import Graphics.Element (..)
+import Text (..)
+import Color (..)
 
 -- Import reset events from JS
 port reset : Signal ()
@@ -12,6 +19,7 @@ events = merge (Just <~ sampleOn Mouse.clicks Mouse.position)
                (always Nothing <~ reset)
 
 -- Keep a list of stamps, reseting when appropriate
+clickLocations : Signal (List (Int,Int))
 clickLocations =
     let update event locations = case event of
                                    Just loc -> loc :: locations
@@ -19,15 +27,17 @@ clickLocations =
     in  foldp update [] events
 
 -- Show the stamp list on screen
+scene : (Int, Int) -> List (Int, Int) -> Element
 scene (w,h) locs =
   let drawPentagon (x,y) =
-          ngon 5 20 |> filled (hsva (toFloat x) 1 1 0.7)
+          ngon 5 20 |> filled (hsla (toFloat x) 1 0.5 0.7)
                     |> move (toFloat x - toFloat w / 2, toFloat h / 2 - toFloat y)
                     |> rotate (toFloat x)
-  in  layers [ collage w h (map drawPentagon locs)
-             , plainText "Click to stamp a pentagon." ]
+  in  layers [ collage w h (List.map drawPentagon locs)
+             , plainText "Click to stamp a pentagon."
+             ]
 
-main = lift2 scene Window.dimensions clickLocations
+main = map2 scene Window.dimensions clickLocations
 
 -- Export the number of stamps
 port count : Signal Int
