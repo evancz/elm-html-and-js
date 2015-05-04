289742ca7f710838b0fda88dc6c11d3cee36f6ff
Evan Czaplicki <info@elm-lang.org>
2015-05-03 17:19:14 -0700

    Reduce imports, update indentation style

--- Stamps.elm
+++ Stamps.elm
@@ -3,42 +3,53 @@ module Stamps where
 import Color exposing (..)
 import Graphics.Collage exposing (..)
 import Graphics.Element exposing (..)
-import List
-import List exposing ((::))
 import Mouse
-import Signal exposing (..)
-import Text exposing (..)
+import Signal exposing ((<~))
 import Window
+
 
 -- Import reset events from JS
 port reset : Signal ()
 
+
 -- Events can either be mouse clicks or reset events
 events : Signal (Maybe (Int,Int))
-events = merge (Just <~ sampleOn Mouse.clicks Mouse.position)
-               (always Nothing <~ reset)
+events =
+  Signal.merge
+    (Just <~ Signal.sampleOn Mouse.clicks Mouse.position)
+    (always Nothing <~ reset)
+
 
 -- Keep a list of stamps, resetting when appropriate
 clickLocations : Signal (List (Int,Int))
 clickLocations =
-    let update event locations = case event of
-                                   Just loc -> loc :: locations
-                                   Nothing  -> []
-    in  foldp update [] events
+    let update event locations =
+          case event of
+            Just loc -> loc :: locations
+            Nothing  -> []
+    in
+        Signal.foldp update [] events
+
 
 -- Show the stamp list on screen
 scene : (Int, Int) -> List (Int, Int) -> Element
 scene (w,h) locs =
   let drawPentagon (x,y) =
-          ngon 5 20 |> filled (hsla (toFloat x) 1 0.5 0.7)
-                    |> move (toFloat x - toFloat w / 2, toFloat h / 2 - toFloat y)
-                    |> rotate (toFloat x)
-  in  layers [ collage w h (List.map drawPentagon locs)
-             , show "Click to stamp a pentagon."
-             ]
+          ngon 5 20
+            |> filled (hsla (toFloat x) 1 0.5 0.7)
+            |> move (toFloat x - toFloat w / 2, toFloat h / 2 - toFloat y)
+            |> rotate (toFloat x)
+  in
+      layers
+        [ collage w h (List.map drawPentagon locs)
+        , show "Click to stamp a pentagon."
+        ]
 
-main = map2 scene Window.dimensions clickLocations
+
+main =
+  Signal.map2 scene Window.dimensions clickLocations
 
 -- Export the number of stamps
 port count : Signal Int
-port count = List.length <~ clickLocations
+port count =
+  Signal.map List.length clickLocations
